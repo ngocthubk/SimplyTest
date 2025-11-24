dbf637858c80e6f22945adb10a188c453b52b1e4
Administrator <ngocthubk@gmail.com>
2025-11-24 15:20:00 +0100

    Fix inputQuantityOfProduct Change the usesage of clear() to press()

--- helpers/page-objects/cart.ts
+++ helpers/page-objects/cart.ts
@@ -53,8 +53,8 @@ export class Cart{
        */
     async inputQuantityOfProduct(product: string, quantity: number){
         let ctQuantity = await this.getQuantityElement(product);
-        await ctQuantity.clear()
-        await ctQuantity.pressSequentially(quantity.toString(),{delay: 500});        
+        await ctQuantity.press('Delete')
+        await ctQuantity.pressSequentially(quantity.toString(),{delay: 500});                
         
     }
 
